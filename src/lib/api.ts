4444c8aef0b6680d038c2efebad45d5302373848
Ghost404 <djoaquimnamueto@gmail.com>
2025-08-31 15:50:44 +0200

    profile

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = "https://bizlink-production.up.railway.app";
 
 export type LoginResponse = {
   access_token: string;
